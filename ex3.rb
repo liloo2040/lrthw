00b5a0bf1eab8583447dbb9558fc46499edd2a6d
liloo2040 <e.dufour.89@gmail.com>
2016-02-04 16:40:47 +0100

    ex

---
+++ ex3.rb
@@ -0,0 +1,27 @@
+# adding a text
+puts "I will now count my chickens:"
+# addition + division
+puts "Hens #{25 + 30 / 6}"
+# multiplier diviser
+puts "Roosters #{100 - 25 * 3 % 4}"
+# adding a text
+puts "Now I will count the eggs:"
+#
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# greater than
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+#greater
+puts "Is it greater? #{5 > -2}"
+#greater
+puts "Is it greater or equal? #{5 >= -2}"
+#lower
+puts "Is it less or equal? #{5 <= -2}"
